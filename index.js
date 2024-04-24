a377255371fff70195a6d2a2b708ea473df04b42
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2024-04-24 14:44:55 -0300

    tradução do console.log

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use('/deliveries', deliveryRoutes);
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`O servidor está rodando na porta ${PORT}`);
 });
