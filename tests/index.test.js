01be3aced74416c23811b8cbf4250ac656aea0b9
Wanderson Reis <wasare@gmail.com>
2024-05-08 14:51:05 -0300

    fix: tudo

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,84 +1,92 @@
-const request = require('supertest')
-const baseUrl = 'http://localhost:3000/deliveries'
+const request = require('supertest');
+const express = require('express');
 
+// const baseUrl = 'http://127.0.0.1:3000';
+const app = express();
 
-describe('GET /:trackingNumber - Obter informação do rastreio', () => {
-    const newDelivery = {
-        trackingNumber: '10',
-        status: 'Pedido em preparação'
-    }
+const routes = require('../routes/deliveryRoutes');
+
+app.use(express.json());
+app.use('/deliveries', routes);
+
+describe('POST - cria um rastreio',  () => {
 
     beforeAll(async () => {
-        await request(baseUrl).post('/').send(newDelivery)
-    })
+        await request(app).delete('/deliveries/HY787');
+    }); // setup do test
 
     afterAll(async () => {
-        await request(baseUrl).delete(`/${newDelivery.trackingNumber}`)
-    })
+        // excluir o que criei
+        await request(app).delete('/deliveries/HY787');
+    });
 
-    it('Deve retornar o delivery 10', async () => {
-        const response = await request(baseUrl).get(`/${newDelivery.trackingNumber}`);
-        expect(response.statusCode).toBe(200)
-        expect(response.body.trackingNumber).toBe('10');
-    })
+    it('Deve criar um rastreio', async () => {
+        const rastreio = await request(app).post('/deliveries').send({trackingNumber: "HY787", status: "Pedido recebido" });
 
-    it('Deve retornar "Delivery not found"', async () => {
-        const response = await request(baseUrl).get(`/0`);
-        expect(response.statusCode).toBe(404)
-        expect(response.body.error).toBe('Delivery not found');
-    })
+
+        expect(rastreio.statusCode).toBe(201);
+        expect(rastreio.body.trackingNumber).toBe("HY787");
+        expect(rastreio.body.status).toBe("Pedido recebido");
+    });
+
+    it('Não deve criar rastreio com mesmo tracking number', async () => {
+        const rastreio = await request(app).post('/deliveries').send({trackingNumber: "HY787", status: "Pedido recebido" });
+
+        expect(rastreio.statusCode).toBe(500);
+    });
+
+})
+
+describe('GET - obtém um rastreio', () => {
+
+    beforeAll(async () => {
+        await request(app).post('/deliveries').send({trackingNumber: "HO09", status: "Pedido em transporte"});
+    });
+
+    afterAll(async () => {
+        await request(app).delete('/deliveries/HO09');
+    });
+
+    it('Deve retornar o rastreio criado', async () => {
+        const rastreio = await request(app).get('/deliveries/HO09');
+
+        expect(rastreio.statusCode).toBe(200);
+        expect(rastreio.body.trackingNumber).toBe('HO09');
+        expect(rastreio.body.status).toBe("Pedido em transporte");
+
+    });
+
+    it('Deve retornar não encontrado', async () => {
+        const rastreio = await request(app).get('/deliveries/6464JDJTETELDKD');
+
+        expect(rastreio.statusCode).toBe(404);
+    });
+
 });
 
-describe('POST / - Criando um rastreio', () => {
-    const newDelivery = {
-        trackingNumber: '10',
-        status: 'Saiu para entrega'
-    }
-
-    afterAll(async () => {
-        await request(baseUrl).delete(`/${newDelivery.trackingNumber}`)
-    })
-
-    it('Deve retornar o objeto criado', async () => {
-        const response = await request(baseUrl).post('/').send(newDelivery)
-        expect(response.statusCode).toBe(200)
-        expect(response.body.trackingNumber).toBe(newDelivery.trackingNumber)
-    })
-
-    it('Deve retornar "Failed to create delivery"', async () => {
-        const response = await request(baseUrl).post('/').send('dados inválidos')
-        expect(response.statusCode).toBe(500)
-        expect(response.body.error).toBe('Failed to create delivery')
-    })
-})
-
-describe('/PUT /:trackingNumber - Atualiza as informações do rastreio', () => {
-    const newDelivery = {
-        trackingNumber: '10',
-        status: 'Saiu para entrega'
-    }
-
-    const newStatus = {
-        status: 'Entregue'
-    }
+describe('PUT - atualiza um rastreio', () => {
 
     beforeAll(async () => {
-        await request(baseUrl).post('/').send(newDelivery)
-    })
+        await request(app).post('/deliveries').send({trackingNumber: "UT987", status: "Pedido em transporte"});
+    });
 
     afterAll(async () => {
-        await request(baseUrl).delete(`/${newDelivery.trackingNumber}`)
-    })
+        await request(app).delete('/deliveries/UT987');
+    });
 
-    it('Deve retornar o objeto atualizado', async () => {
-        const response = await request(baseUrl).put(`/${newDelivery.trackingNumber}`).send(newStatus)
-        expect(response.statusCode).toBe(200)
-        expect(response.body.status).toBe(newStatus.status)
-    })
+    it("Deve atualizar o status do rastreio", async () => {
+        const rastreio = await request(app).put('/deliveries/UT987').send({trackingNumber: 'UT987', status: "endereço não localizado"});
 
-    it('Deve retornar "Delivery not found"', async () => {
-        const response = await request(baseUrl).put('/0').send(newStatus.status)
-        expect(response.statusCode).toBe(404)
-        expect(response.body.error).toBe('Delivery not found')
-    })
-})
+        expect(rastreio.statusCode).toBe(200);
+        expect(rastreio.body.trackingNumber).toBe("UT987");
+        expect(rastreio.body.status).toBe("endereço não localizado");
+    });
+
+    it("Deve retornar rastreio não encontrado", async () => {
+        const rastreio = await request(app).put('/deliveries/HDHDG9856756').send({trackingNumber: 'HDHDG9856756', status: "endereço não localizado"});
+
+        expect(rastreio.statusCode).toBe(404);
+
+    });
+
+});
